413cd0754a7d4f1691d7345b69a12087695e068d
ZanyaMyers <myerszanya@gmail.com>
2023-11-07 12:50:37 -0800

    selection2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch(subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is my least favorite subject");
+                break;
+                case "History":
+                    Console.WriteLine("History is my favorite subject");
+                    break;
+                case "English":
+                    Console.WriteLine("English is fun!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a hard subject");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I have not taken that subject. {subject} sounds fun!");
+                    break; 
+
+
+
+           
+            }
+       
         }
     }
 }
